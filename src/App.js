a5a0647098418977b16e274d3d3e5645e0c923ae
Uvejs Mikullovci <mikullovciuvejs@gmail.com>
2025-10-19 23:30:05 +0200

    Made folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom/BrowserRouter";
 
 function App() {
-  return <div className="App">lamziiiiiiiiiii</div>;
+  return <div className="App">
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Import HomePage</h1>} />
+      </Routes>
+    </Router>
+  </div>;
 }
 
 export default App;
