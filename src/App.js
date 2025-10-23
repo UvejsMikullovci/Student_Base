601efb59776df271a66a9ebfe6efd0eef9f6cb87
drijajtmerovci <drijajt.merovcii5@gmail.com>
2025-10-23 23:07:29 +0200

    Created settings in panel area and also created a dropdown element that has values of the language and timezone you choose,and i also created a toggleON and OFF button,IOS like,that you guys can use and its in atoms of group4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/organisms/group5/Header";
 import "./components/styles/variables.css"
+import Favorited from "./components/pages/group4/favorited";
+import Settings from "./components/pages/group4/settings";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<h1>Import HomePage</h1>} />
+          <Route path="/panel-favorited" element={<Favorited/>} />
+          <Route path="/panel-settings" element={<Settings/>} />
         </Routes>
       </Router>
     </div>
--- src/components/pages/group4/favorited.js
+++ src/components/pages/group4/favorited.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function favorited() {
   return (
-    <div>favorited</div>
+    
+    <div>
+    </div>
   )
 }
 
