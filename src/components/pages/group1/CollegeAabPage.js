b82bb97070b09594ac4fcda30a031fe090cd436f
Gresa Qunglla <gresaqunglla@gmail.com>
2025-10-24 20:13:26 +0200

    test
    test

---
+++ src/components/pages/group1/CollegeAabPage.js
@@ -0,0 +1,11 @@
+// src/pages/group1/CollegeAabPage.js
+import React from 'react';
+import CollegeAabSection from '../../organisms/group1/CollegeAabSection';
+
+export default function CollegeAabPage() {
+  return (
+    <div>
+      <CollegeAabSection />
+    </div>
+  );
+}
