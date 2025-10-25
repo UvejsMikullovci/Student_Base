6e2a13cfcf2f3f218ad21336aefff36ca56c5bb3
aulona0ss <aulona123malta@gmail.com>
2025-10-25 11:58:46 +0200

    Why Choose Us
    Second banner completed

--- src/components/pages/group1/HomePage.jsx
+++ src/components/pages/group1/HomePage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import {HeroSection} from "../../organisms/group1/HeroSection";
 import "./HomePage.css"
+import { WhyChooseSection } from "../../organisms/group1/WhyChooseSection";
 
 export function HomePage() {
   return (
     <div className="home-page">
       <HeroSection />
+      <WhyChooseSection/>
     </div>
+    
   );
 }
 
