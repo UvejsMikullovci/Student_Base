52a85d3fa6b1cd0e2d7d89cb8cbd99983cf85ac5
lamziii <malklinaku9@gmail.com>
2025-10-25 18:22:04 -0400

    Created the user profile, fast statistics and profile completed percentage

--- src/components/molecules/group4/profiliIm/FastStatistics.js
+++ src/components/molecules/group4/profiliIm/FastStatistics.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
-export default function FastStatistics() {
+export default function FastStatistics({title, headingOne, headingTwo, headingThree,headingFour}) {
   return (
-    <div>
-      
+    <>
+      <div className='div-fastStatistics'>
+      <div className='div-profileBoxTitle'>
+      <h3>{title}</h3>
+       </div>
+        <div className='div-stats'>
+        <div className='div-profileBoxText'>
+        <h4>{headingOne}</h4>
+         <h4>{headingTwo}</h4>
+          <h4>{headingThree}</h4>
+           <h4>{headingFour}</h4>
+        </div>
+        <div className='div-profileBoxNumber'>
+        <h3>5</h3>
+        <h3>2</h3>
+        <h3>3</h3>
+        <h3>4/5</h3>
+        </div>
+      </div>
     </div>
+    </>
+
   )
 }
--- src/components/organisms/group4/profiliIm/RightSide.js
+++ src/components/organisms/group4/profiliIm/RightSide.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Profile from '../../../molecules/group4/profiliIm/ProfileBox'
+import FastStatistics from '../../../molecules/group4/profiliIm/FastStatistics'
 import './RightSide.css'
 
 export default function RightSide() {
@@ -16,11 +17,13 @@ export default function RightSide() {
         ></Profile>
       </div>
       <div className='div-profileFastStatistics'>
-        <Profile
-          title="sda"
-          text="sadwa"
-          number="wadsad"
-        ></Profile>
+        <FastStatistics
+        title="Statistika te shpejta"
+        headingOne="Aplikime totale"
+        headingTwo="Te pranuara"
+        headingThree="Ne pritje"
+        headingFour="Dokumente"
+        ></FastStatistics>
       </div>
     </div>
 
