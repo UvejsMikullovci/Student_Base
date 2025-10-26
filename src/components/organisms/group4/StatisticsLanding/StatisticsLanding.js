29e25403cc5667f4c28822b8dca8bdd3752dd218
Benetimeri <Benetimeri455@gmail.com>
2025-10-26 23:39:01 +0100

    Statistics Page
    Started development on StatisticsPage, installed lucide - icon library , refer to:https://lucide.dev

---
+++ src/components/organisms/group4/StatisticsLanding/StatisticsLanding.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./StatisticsLanding.css";
+import StatisticsBasicDiv from "../../../molecules/group4/statisticsDiv/StatisticsBasicDiv";
+import { TrendingUp, Clock, Target, Users, Award } from "lucide-react";
+import "./StatisticsLanding.css";
+
+function StatisticsLanding() {
+  return (
+    <div>
+      <div className="statistics-text">
+        <h2>Statistikat</h2>
+        <p>Shiko performancën dhe progresionin tënd</p>
+      </div>
+      <div className="statistics-container">
+        <StatisticsBasicDiv
+          value="8.6"
+          label="Mesatarja juaj"
+          change="+0.4"
+          changeColor="text-green-600"
+          iconName="<TrendingUp />"
+        />
+        <StatisticsBasicDiv
+          value="3/45"
+          label="Vendi në klasë"
+          change="+2"
+          changeColor="text-green-600"
+          iconName="<Clock />"
+        />
+        <StatisticsBasicDiv
+          value="12/15"
+          label="Objektivat e arritura"
+          change="+0.4"
+          changeColor="text-green-600"
+          iconName="<Target />"
+        />
+        <StatisticsBasicDiv
+          value="126h"
+          label="Orë studimi"
+          change="+18h"
+          changeColor="text-green-600"
+          iconName="<Clock />"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default StatisticsLanding;
---
+++ src/components/pages/group4/panelStatistics/StatisticsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StatisticsLanding from "../../../organisms/group4/StatisticsLanding/StatisticsLanding";
+
+function StatisticsPage() {
+  return (
+    <div>
+      <StatisticsLanding />
+    </div>
+  );
+}
+
+export default StatisticsPage;
