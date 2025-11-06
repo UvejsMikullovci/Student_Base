ab74135758be817ea48682bad01a124b885c5739
Uvejs Mikullovci <mikullovciuvejs@gmail.com>
2025-11-06 22:32:06 +0100

    Fixed errors

--- src/components/pages/panelProf/panelProfMain/PanelProf.js
+++ src/components/pages/panelProf/panelProfMain/PanelProf.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import { User, FileText, Home, Settings } from "lucide-react";
+import { User, Megaphone, GraduationCap, Settings, PencilLine } from "lucide-react";
 import Sidebar from "../../../organisms/group5/Sidebar";
 import PanelProfProfile from "../panelProfProfile/PanelProfProfile";
 import PanelProfStudents from "../panelProfStudents/PanelProfStudents";
 import PanelProfGrades from "../panelProfGrades/PanelProfGrades";
-import PanelProfAnnouncements from "../panelProfAnnouncements/PanelProfAnnouncements";
+import PanelProfAnnouncements from "../panelProfAnnouncments/PanelProfAnnouncments";
 import PanelProfSettings from "../panelProfileSettings/PanelProfileSettings";
 import "../../../organisms/group5/Sidebar.css";
-import "../panelProf.css";
+import "../panelProfMain/PanelProf.css";
+
 
 export default function PanelDormsMainContainer() {
   const [activePanel, setActivePanel] = useState("Profili im");
@@ -15,10 +16,10 @@ export default function PanelDormsMainContainer() {
   // 🏠 Sidebar menu for dorm panel
   const dormMenu = [
     { title: "Profili im", icon: <User size={18} /> },
-    { title: "Nxenesat", icon: <Home size={18} /> },
-    { title: "Notimi", icon: <FileText size={18} /> },
-    { title: "Njoftimet", icon: <Settings size={18} /> },
-    { title: "Cilesimet", icon: <Settings size={18} /> },
+    { title: "Nxenesat", icon: <GraduationCap size={18} /> },
+    { title: "Notimi", icon: <PencilLine size={18} />},
+    { title: "Njoftimet", icon: <Megaphone size={18} /> },
+    { title: "Cilesimet", icon: <Settings size={18} />},
   ];
 
   // Render sections
